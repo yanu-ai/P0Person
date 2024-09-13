62c14863af7f5c55b9c23a5d5c3b50877f292463
Iyanu Folarin <iyanu.folarin@duke.edu>
2024-09-12 23:51:22 -0400

    version2

--- src/Person201Demo.java
+++ src/Person201Demo.java
@@ -2,7 +2,7 @@ public class Person201Demo {
     public static void main(String[] args) {
         Person201 a = new Person201("claire",37.8044,-122.2712,"Blue Corn Cafe");
         Person201 b = new Person201("ricardo",-1.2921, 36.8219,"Elmo's Diner");
-        Person201 c = new Person201("julie",40.7081,-73.9571,"Alpaca");
+        Person201 c = new Person201();
         Person201 s = new Person201("Ethan", 40.6782, 73.0442, "Monuts");
         Person201[] datas = {a,b,c,s};
 
@@ -10,7 +10,7 @@ public class Person201Demo {
             System.out.println(p);
         }
 
-        System.out.printf("names: %s, %s, %s\n",a.name(),b.name(),c.name());
+        System.out.printf("names: %s, %s, %s, %s\n",a.name(),b.name(),c.name(),s.name());
 
         Person201 p = new Person201("Sam", 38.6, 90.19, "Common Market");                               
         Person201 q = new Person201("Fred", 41.88, 87.63, "Local 22");                             
